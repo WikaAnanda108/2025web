bbdc32e20f574faa07e42ac0f1ac76659800d7a7
Wika Ananda <wikaanandaaa@gmail.com>
2025-04-28 16:53:48 +0800

    login

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+if (!isset($_SESSION['login'])) {
+    header("location:login.html");
+}
 include "koneksi.php";
 
 $query = "SELECT m.*, p.nama namaProdi FROM mahasiswa m JOIN prodi p ON m.id_prodi = p.id";
@@ -10,45 +14,45 @@ $data = ambildata($query);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIMPADU POLIBAN</title>
+    <title>SIMPADU</title>
 </head>
 <body>
-<h1>DATA MAHASISWA</h1>
+    <h1>DATA MAHASISWA</h1>
     <br>
-    <a href="tambahmahasiswa.php">Tambah Data</a>
-     <table border="1" cellspacing="0" cellpadding"1">
-        <thread>
-            <th>no</th>
+    <a href = "tambahmahasiswa.php">tambah</a>
+    <table border="1" cellspacing="0" cellpadding = "1">
+        <thead>
+        <tr>
+            <th>No</th>
             <th>nim</th>
             <th>nama</th>
             <th>tanggal_lahir</th>
             <th>telp</th>
             <th>email</th>
             <th>prodi</th>
-            <th>Aksi</th>
-            
-    </thread>
-        <tbody>
-            <?php
-            $i=1;
-            foreach($data as $d) : ?>
+            <th>aksi</th>
+    </thead>
+    <tbody>
+
+    <?php
+    $i = 1;
+    foreach ($data as $d) : ?>
 
     <tr>
-        <td><?= $i++ ?></td>
-        <td><?= $d ["nim"] ?></td>
-        <td><?= $d ["nama"] ?></td>
-        <td><?= $d ["tanggal_lahir"] ?></td>
-        <td><?= $d ["telp"] ?></td>
-        <td><?= $d ["email"] ?></td>
-        <td><?= $d ["namaProdi"] ?></td>
-        <td><a href="deletemahasiswa.php?nim=<?= $d['nim']; ?>"
-        onclick="return confirm('Yakin nihh?')">Delete</a> |
-        <a href="editmahasiswa.php?nim=<?= $d['nim']; ?>">Edit</a></td>
-            </tr>
-<?php endforeach ; ?>
-
-
-<tbody>
- </table>   
+    <td><?= $i++; ?></td>
+    <td><?= $d["nim"] ?></td>
+    <td><?= $d["nama"] ?></td>
+    <td><?= $d["tanggal_lahir"] ?></td>
+    <td><?= $d["telp"] ?></td>
+    <td><?= $d["email"] ?></td>
+    <td><?= $d["namaProdi"] ?></td>
+    <td><a href="deletemahasiswa.php?nim=<?= $d['nim']; ?>"
+    onclick="return confirm('Yakin ingin hapus?')">Delete</a> |
+    <a href="editmahasiswa.php?nim=<?= $d['nim']; ?>">Edit</a></td>
+    </tr>
+    <?php endforeach; ?>
+        </tbody>
+    </table>
+    <a href = "logout.php">Logout</a>
 </body>
 </html>
